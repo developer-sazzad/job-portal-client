11d394047ac46d5c355e8d01781b235dfd7aeade
Sazzad Hossain <developersazzad@gmail.com>
2025-03-12 04:13:14 +0600

    banner section animation and responsive

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -10,9 +10,28 @@ const Banner = () => {
 
 
             <div class="container mx-auto items-center overflow-visible">
-                <div class="flex gap-10 justify-center items-center flex-col py-32 lg:flex-row-reverse">
+                <div class="lg:flex gap-10 justify-center items-center">
                     <div className="flex-1">
-                        <div className="flex flex-col justify-center">
+                        <h1 class="text-7xl font-bold">The <motion.span
+                            animate={{ color: ['#002bff', '#04ff00', '#ff002b'] }}
+                            transition={{ duration: 5, repeat: Infinity }}
+                            className="bg-linear-to-t from-[#DFE7FC] px-4"
+                        >Easiest Way
+                        </motion.span>
+                            <br /> to Get Your New Job</h1>
+                        <p class="py-6 text-2xl">
+                            Each month, more than 3 million job seekers turn to website in their search for work, making over 140,000 applications every single day
+                        </p>
+                        <div className="flex my-5">
+                            <input className="input input-xl" type="text" placeholder="input" />
+                            <input className="input input-xl" type="text" placeholder="input" />
+                            <input className="input input-xl" type="text" placeholder="input" />
+                            <input className='btn btn-xl' type="submit" value="Search" />
+                        </div>
+                        <p className="py-5">Popular Searches: Content Writer , Finance , Human Resource , Management</p>
+                    </div>
+                    <div className="flex-1">
+                        <div className="flex flex-col justify-center py-20 lg:py-20">
                             <motion.img
                                 animate={{ y: [-50, 50, -50] }}
                                 transition={{ duration: 10, ease: easeInOut, repeat: Infinity }}
@@ -24,24 +43,6 @@ const Banner = () => {
                                 src={team2}
                                 class="max-w-md -mt-16 rounded-t-[50px] rounded-br-[50px] border-b-8 border-l-8 border-blue-600 shadow-2xl" />
                         </div>
-                    </div>
-                    <div className="flex-1">
-                        <h1 class="text-6xl font-bold">The <motion.span
-                            animate={{ color: ['#002bff', '#04ff00', '#ff002b'] }}
-                            transition={{ duration: 5, repeat: Infinity }}
-                            className="bg-linear-to-t from-[#DFE7FC] px-4"
-                        >Easiest Way
-                        </motion.span>
-                            <br /> to Get Your New Job</h1>
-                        <p class="py-6 text-2xl">
-                            Each month, more than 3 million job seekers turn to website in their search for work, making over 140,000 applications every single day
-                        </p>
-                        <div className="flex">
-                            <input className="input" type="text" placeholder="input" />
-                            <input className="input" type="text" placeholder="input" />
-                            <input className='btn' type="submit" value="Search" />
-                        </div>
-                        <p className="py-5">Popular Searches: Content Writer , Finance , Human Resource , Management</p>
                     </div>
                 </div>
             </div>
--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,6 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/icons/logo-small.png'
 
 const Header = () => {
+
+    const links = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+    </>
     return (
         <div className="container mx-auto navbar">
             <div className="navbar-start">
@@ -11,15 +19,7 @@ const Header = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {links}
                     </ul>
                 </div>
                 <div className='flex items-center gap-3'>
@@ -28,22 +28,12 @@ const Header = () => {
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul className="menu menu-horizontal text-xl font-medium px-1">
+                    {links}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn btn-neutral">Button</a>
             </div>
         </div>
     );
